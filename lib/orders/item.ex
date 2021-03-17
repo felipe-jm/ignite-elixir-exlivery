42e31c7d260e5cea0c28305379f32c33d69fe385
Felipe Jung <felipemattoseu@gmail.com>
2021-03-17 08:52:14 -0400

    add order build_item function

--- lib/orders/item.ex
+++ lib/orders/item.ex
@@ -8,6 +8,10 @@ defmodule Exlivery.Orders.Item do
 
   def build(description, category, unity_price, quantity)
       when quantity > 0 and category in @categories do
+    unity_price
+    |> Decimal.cast()
+    |> build_item(description, category, quantity)
+
     {:ok,
      %__MODULE__{
        description: description,
@@ -18,4 +22,16 @@ defmodule Exlivery.Orders.Item do
   end
 
   def build(_description, _category, _unity_price, _quantity), do: {:error, "Invalid parameters"}
+
+  defp build_item({:ok, unity_price}, description, category, quantity) do
+    {:ok,
+     %__MODULE__{
+       description: description,
+       category: category,
+       unity_price: unity_price,
+       quantity: quantity
+     }}
+  end
+
+  defp build_item(:error, _description, _category, _quantity), do: {:error, "Invalid price"}
 end
