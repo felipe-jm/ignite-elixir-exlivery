e008cb943d21d555fb217fdd1150857f320c1fe3
Felipe Jung <felipemattoseu@gmail.com>
2021-03-17 09:43:11 -0400

    test: add orders test

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       quantity: 1,
       unity_price: Decimal.new("35.5")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua 35",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Risoto com queijo",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("30.50")
+        )
+      ],
+      total_price: Decimal.new("96.50"),
+      user_cpf: "12312312312"
     }
   end
 end
