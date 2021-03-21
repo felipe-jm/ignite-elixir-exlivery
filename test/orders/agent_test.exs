082acd487a3c38e26351b66cae8b6772a8685da0
Felipe Jung <felipemattoseu@gmail.com>
2021-03-21 11:16:55 -0400

    Test orders agent

---
+++ test/orders/agent_test.exs
@@ -0,0 +1,46 @@
+defmodule Exlivery.Orders.AgentTest do
+  use ExUnit.Case
+
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.{Item, Order}
+
+  import Exlivery.Factory
+
+  describe "save/1" do
+    test "saves the order" do
+      OrderAgent.start_link(%{})
+
+      order = build(:order)
+
+      assert {:ok, _uuid} = OrderAgent.save(order)
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      OrderAgent.start_link(%{})
+
+      :ok
+    end
+
+    test "when the order is found, returns the order" do
+      order = build(:order)
+
+      {:ok, uuid} = OrderAgent.save(order)
+
+      response = OrderAgent.get(uuid)
+
+      expected_response = {:ok, order}
+
+      assert response == expected_response
+    end
+
+    test "when the order is not found, returns an error" do
+      response = OrderAgent.get("00000000000")
+
+      expected_response = {:error, "Order not found"}
+
+      assert response == expected_response
+    end
+  end
+end
